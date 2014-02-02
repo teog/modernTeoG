555ea8360391b12217336caa4318eda9594aa363
teog <teo1969@gmail.com>
2014-02-02 23:26:28 +0100

    add files

---
+++ build/appinfo.auto.c
@@ -0,0 +1,20 @@
+#include "pebble_app_info.h"
+#include "src/resource_ids.auto.h"
+
+const PebbleAppInfo __pbl_app_info __attribute__ ((section (".pbl_header"))) = {
+  .header = "PBLAPP",
+  .struct_version = { APP_INFO_CURRENT_STRUCT_VERSION_MAJOR, APP_INFO_CURRENT_STRUCT_VERSION_MINOR },
+  .sdk_version = { APP_INFO_CURRENT_SDK_VERSION_MAJOR, APP_INFO_CURRENT_SDK_VERSION_MINOR },
+  .app_version = { 1, 0 },
+  .load_size = 0xb6b6,
+  .offset = 0xb6b6b6b6,
+  .crc = 0xb6b6b6b6,
+  .name = "Modern",
+  .company = "TeoG",
+  .icon_resource_id = RESOURCE_ID_IMAGE_MENU_ICON,
+  .sym_table_addr = 0xA7A7A7A7,
+  .flags = APP_INFO_WATCH_FACE,
+  .num_reloc_entries = 0xdeadcafe,
+  .uuid = { 0xB0, 0x3F, 0x16, 0xA0, 0x7C, 0xBB, 0x11, 0xE3, 0xBA, 0xA7, 0x08, 0x00, 0x20, 0x0C, 0x9A, 0x66 },
+  .virtual_size = 0xb6b6
+};
